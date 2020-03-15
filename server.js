d76115aac35a8a8795d929cbaeac3c0736e6a037
Olivia Davis <olivia.davis593@gmail.com>
2020-03-14 23:46:05 -0500

    setting app up for deployment

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const MOVIES = require('./movie-data.json')
 
-console.log(process.env.API_TOKEN)
+//console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
 //only used when initializing express app 
 /*
@@ -15,6 +21,7 @@ app.use((req, res) => {
     res.send('Hello, world!')
 })
 */
+
 
 app.use(function validateBearerToken(req, res, next) {
     console.log('validate bearer token middleware')
@@ -68,7 +75,18 @@ app.get('/movie', function handleGetMovie(req, res) {
 
 })
 
-const PORT = 8000
+//friendly error-handling middleware 
+app.use((error, req, res, next)  => {
+    let response 
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
