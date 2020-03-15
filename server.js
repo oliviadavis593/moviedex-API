911b803a0fdf1b59c4f7e86f23ad10dd70a41e75
Olivia Davis <olivia.davis593@gmail.com>
2020-03-14 23:04:29 -0500

    validation fully implemented

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const MOVIES = require('./movie-data.json')
 
 console.log(process.env.API_TOKEN)
 
@@ -42,11 +43,30 @@ function handleGetGenre(req, res) {
 
 app.get('/genre', handleGetGenre)
 
-function handleGetCountry(req, res) {
-    console.log('Hello Im the country')
-}
+app.get('/movie', function handleGetMovie(req, res) {
+    let response = MOVIES; 
 
-app.get('/country', handleGetCountry)
+    if (req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())    
+        )
+    }
+
+    if (req.query.country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())    
+        )
+    }
+
+    if (req.query.avg_vote) {
+        response = response.filter(movie => 
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)    
+        )
+    }
+
+    res.json(response)
+
+})
 
 const PORT = 8000
 
